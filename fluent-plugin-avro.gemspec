c6b6bec1ac1285043846909b127f113d6bfde306
Shun Takebayashi <shun@takebayashi.asia>
2015-06-11 23:10:18 +0900

    Implement AvroFormatter#format

--- fluent-plugin-avro.gemspec
+++ fluent-plugin-avro.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "avro"
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_runtime_dependency "fluentd", "~> 0.12.0"
--- lib/fluent/plugin/avro.rb
+++ lib/fluent/plugin/avro.rb
@@ -1,6 +1,24 @@
+require 'avro'
+
 module Fluent
   module TextFormatter
     class AvroFormatter < Formatter
+      Fluent::Plugin.register_formatter('avro', self)
+
+      config_param :schema_file, :string
+
+      def configure(conf)
+        super
+        @schema = Avro::Schema.parse(File.read(@schema_file))
+      end
+
+      def format(tag, time, record)
+        writer = Avro::IO::DatumWriter.new(@schema)
+        buffer = StringIO.new
+        encoder = Avro::IO::BinaryEncoder.new(buffer)
+        writer.write(record, encoder)
+        buffer.string
+      end
     end
   end
 end
