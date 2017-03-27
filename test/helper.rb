5576093be180053224dd6627a63e6d6c467f5c61
Hiroshi Hatake <hatake@clear-code.com>
2017-03-27 16:22:19 +0900

    Migrate to use v0.14 API

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require 'fluent/log'
 require 'fluent/test'
+require 'fluent/test/driver/formatter'
 
 unless defined?(Test::Unit::AssertionFailedError)
   class Test::Unit::AssertionFailedError < StandardError
